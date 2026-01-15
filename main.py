3c5822e33e699b73e0091abce677249d1df2c373
Konstantin <geimonen68@gmail.com>
2026-01-15 17:41:36 +0200

    added patch method to update the task

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI, HTTPException
 
+from config import TaskModel, TaskUpdate
+
 
 app = FastAPI()
 
 
 tasks = []
 
-
+# получить все задачи
 @app.get("/tasks")
 async def get_tasks():
     return tasks
@@ -20,12 +22,25 @@ async def get_task(task_id: int):
         
         
 @app.post("/tasks")
-async def create_task(task: dict):
+async def create_task(task: TaskModel):
     task_id = len(tasks)
     new_task = {
         "id": task_id,
-        "task": task["task"]
+        "task": task.task
     }
     
     tasks.append(new_task)
     return new_task
+
+
+# частичное обновление задачи
+@app.patch("/tasks/{task_id}")
+async def update_task(task_id: int, task: TaskUpdate):
+    if task_id >= len(tasks) or task_id < 0:
+        raise HTTPException(status_code=..., detail="Task not found")
+    else:
+        if task.task is None:
+            raise HTTPException(status_code=400)
+        else:
+            tasks[task_id]["task"] = task.task
+            return tasks[task_id]
